424dc01fdea6dd8331e08a32db69c7e5531f26ff
Xinyang Zhang <xinyang.zhang@hpe.com>
2018-02-01 16:13:07 +0800

    persist to localStorage

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+import throttle from 'lodash/throttle';
+import { loadState, saveState } from './localStorage';
 
-const store = createStore(todoApp);
+const persistedState = loadState();
+const store = createStore(
+  todoApp,
+  persistedState
+);
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos,
+  });
+}, 1000));
 
 render(
   <Provider store={store}>
