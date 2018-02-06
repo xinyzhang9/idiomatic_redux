95e2fcccb2a3b5563698a2aeb4ea4cf5fa5df515
Xinyang Zhang <xinyang.zhang@hpe.com>
2018-02-06 17:44:01 +0800

    colocating selectors wth reducers

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -6,7 +6,7 @@ const FilterLink = ({ filter, children }) => (
     to={filter === 'all' ? '' : filter}
     activeStyle={{
       textDecoration: 'none',
-      color: 'black',
+      color: 'orange',
     }}
   >
     {children}
--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,36 +2,24 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
-
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter(t => t.completed);
-    case 'active':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
+import { getVisibleTodos } from '../reducers';
 
 const mapStateToProps = (state, { params }) => ({
   todos: getVisibleTodos(
-    state.todos,
+    state,
     params.filter || 'all'
   ),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onTodoClick(id) {
-    dispatch(toggleTodo(id));
-  },
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   onTodoClick(id) {
+//     dispatch(toggleTodo(id));
+//   },
+// });
 
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
+  { onTodoClick: toggleTodo }
 )(TodoList));
 
 export default VisibleTodoList;
