9b3c22178652f2146d7e49f54de555cff4027a77
Xinyang Zhang <xinyang.zhang@hpe.com>
2018-02-06 16:55:06 +0800

    withRouter()

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
 
@@ -15,10 +16,10 @@ const getVisibleTodos = (todos, filter) => {
   }
 };
 
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = (state, { params }) => ({
   todos: getVisibleTodos(
     state.todos,
-    ownProps.filter
+    params.filter || 'all'
   ),
 });
 
@@ -28,9 +29,9 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-const VisibleTodoList = connect(
+const VisibleTodoList = withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(TodoList);
+)(TodoList));
 
 export default VisibleTodoList;
