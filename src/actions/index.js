37650fac1e25ff45005b4522297e4d9325ae2b27
Xinyang Zhang <xinyang.zhang@hpe.com>
2018-02-09 17:43:26 +0800

    temp save

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
 import { v4 } from 'node-uuid';
+import * as api from '../api';
 
+const receiveTodos = (filter, response) => ({
+  type: 'RECEIVE_TODOS',
+  filter,
+  response,
+});
+
+export const fetchTodos = (filter) =>
+  api.fetchTodos(filter).then(response =>
+    receiveTodos(filter, response)
+  );
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
   id: v4(),
@@ -9,11 +20,5 @@ export const addTodo = (text) => ({
 export const toggleTodo = (id) => ({
   type: 'TOGGLE_TODO',
   id,
-});
-
-export const receiveTodos = (filter, response) => ({
-  type: 'RECEIVE_TODOS',
-  filter,
-  response,
 });
 
